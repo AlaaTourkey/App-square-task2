4caea78b664cc636ff8d98171bc0e85193b17ede
Alaa Tourkey <alaatourkey@gmail.com>
2024-08-20 03:20:04 +0300

    end task

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,67 +1,151 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 
 function Home() {
 
   const [showPassword, setShowPassword] = useState(false);
-
   const [selectedImage, setSelectedImage] = useState(null);
+  const [message, setmessage] = useState();
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    password: '',
+  });
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
-    console.log(e);
-    
     if (file) {
-      const imageUrl = URL.createObjectURL(file);
-      setSelectedImage(imageUrl);
+      setSelectedImage(file);
     }
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    const formDataToSend = new FormData();
+    formDataToSend.append('image', selectedImage);
+    formDataToSend.append('name', formData.name);
+    formDataToSend.append('email', formData.email);
+    formDataToSend.append('phone', formData.phone);
+    formDataToSend.append('password', formData.password);
+
+    try {
+      const { data } = await axios.post('https://www.appssquare.sa/api/submit', formDataToSend);
+
+      console.log(data);
+      setmessage(data.message)
+
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  };
+
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormData({
+  //     ...formData,
+  //     [name]: value,
+  //   });
+  // };
+  function handleChange({ target }) {
+    setFormData({
+      ...formData, [target.name]: target.value
+    })
+  }
+  console.log(formData);
+  console.log(formData);
   return (
     <>
-      <div className="my-10  py-10 bg-gray-900 grid grid-cols-2">
-        <form className="max-w-sm mx-20 col-span-1">
-          <div className="mb-5">
-            <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
-            <input type="text" id="name" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="enter your name" required />
-          </div>
-          <div className="mb-5">
-            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-            <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@gmail.com" required />
-          </div>
-          <div className="mb-5">
-            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-            <div className="relative">
+      <div className=" text-white ">
+        {message && <div className="text-center alert text-3xl p-3 my-3 ">
+          {message}
+        </div>}
+        <div className="my-5 py-5  grid grid-cols-1 md:grid-cols-2">
+
+          <form onSubmit={handleSubmit} className="max-w-sm mx-10 col-span-1">
+            <h1 className='font-bold text-xl my-5'>REGESTRATION FORM </h1>
+            <div className="mb-5">
+              <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
               <input
-                type={showPassword ? "text" : "password"}
-                id="password"
-                placeholder='enter your password'
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                placeholder="Enter your name"
+                required
+              />
+            </div>
+            <div className="mb-5">
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                placeholder="name@gmail.com"
+                required
+              />
+            </div>
+            <div className="mb-5">
+              <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+              <div className="relative">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  placeholder="Enter your password"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                  required
+                />
+                <button
+                  type="button"
+                  className="absolute top-1/2 text-white -translate-y-1/2 right-2 flex items-center px-2"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? <i className="far fa-eye"></i> : <i className="far fa-eye-slash"></i>}
+                </button>
+              </div>
+            </div>
+            <div className="mb-5">
+              <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your phone</label>
+              <input
+                type="text"
+                id="phone"
+                name="phone"
+                value={formData.phone}
+                onChange={handleChange}
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                placeholder="Enter your phone number"
+                required
+              />
+            </div>
+            <div className="mb-5">
+              <input
+                type="file"
+                onChange={handleImageChange}
                 className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
                 required
               />
-              <button
-                type="button"
-                className="absolute top-1/2 text-white -translate-y-1/2 right-2 flex items-center px-2"
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                {showPassword ? <i className="far fa-eye "></i> : <i className="far fa-eye-slash"></i>}
-              </button>
             </div>
-          </div>
-          <div className="">
-            <input type="file" onChange={handleImageChange} id="email" className="my-5 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
-          </div>
-          <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-        </form>
+            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+          </form>
 
-        <div className="col-span-1 p-10">
-          {selectedImage && (
-            <img
-              src={selectedImage}
-              alt="Selected"
-              className="w-full  object-cover rounded-lg"
-            />
-          )}
+          <div className="col-span-1 p-10">
+            {selectedImage && (
+              <img
+                src={URL.createObjectURL(selectedImage)}
+                alt="Selected"
+                className="w-full object-cover rounded-lg"
+              />
+            )}
+          </div>
         </div>
       </div>
     </>
